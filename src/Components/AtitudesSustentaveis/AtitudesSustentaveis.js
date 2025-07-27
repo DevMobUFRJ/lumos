1982c1a3f2f4cdc7876c01fa481a52f59bc74c98
PedroMion <pedromionbraga@gmail.com>
2025-07-27 17:11:07 -0300

    Finalizando informações do atitudes sustentáveis

--- src/Components/AtitudesSustentaveis/AtitudesSustentaveis.js
+++ src/Components/AtitudesSustentaveis/AtitudesSustentaveis.js
@@ -2,12 +2,29 @@ import './AtitudesSustentaveis.css';
 import { useNavigate } from "react-router-dom";
 import leftArrowImage from "../../Assets/Images/leftArrow.svg";
 import shareImage from "../../Assets/Images/share.svg";
+import InformationPopup from '../InformationPopup/InformationPopup';
 
 const AtitudesSustentaveis = () => {
+    const labcaTitle = "LabCA - Laboratório de Análise e Criação de Ambiências";
+    const labcaText = "Grupo de pesquisa que investiga aspectos sensoriais do espaço voltados para o estímulo da percepção tanto objetiva como subjetiva do usuário que constitui a inter-relação pessoa-ambiente e que colabora na construção do lugar físico, sociológico e fenomenológico, promovendo a identidade espacial. Essas atividades se dão de forma transdisciplinar na geração de conhecimento, trabalhando a promoção do bem estar do ser humano através da ergonomia do mobiliário e dos equipamentos, da relação edifício, cidade e paisagem e da ciência do conforto ambiental, no contexto da sustentabilidade, que envolvem as áreas afins do Design, do Design de Interior, do Paisagismo, da Arquitetura e do Urbanismo. A partir da integração entre ensino, pesquisa e extensão, o LABCA visa à repercussão de seus trabalhos nos meios de divulgação científica, com produção bibliográfica, técnica, artística e cultural que envolva outras Instituições e a Sociedade.";
+    const devmobTitle = "DevMob";
+    const devmobText = "Grupo de Interesse e Extensão em Desenvolvimento de Aplicativos Móveis do Instituto de Computação (IC) da UFRJ - Universidade Federal do Rio de Janeiro.";
+    
     const navigate = useNavigate();
+
     const handleClose = () => {
         navigate('/home');
     };
+
+    const openPopupLabca = () => {
+        document.getElementById('popup-labca').style.visibility = 'visible';
+        document.getElementById('popup-blackout').style.visibility = 'visible';
+    }
+
+    const openPopupDevmob = () => {
+        document.getElementById('popup-devmob').style.visibility = 'visible';
+        document.getElementById('popup-blackout').style.visibility = 'visible';
+    }
 
     return (
         <div className="atitudes-sustentaveis">
@@ -40,13 +57,33 @@ const AtitudesSustentaveis = () => {
                 </p>
 
                 <p className="atitudes-text">
-                    A partir de demandas da sociedade identificadas pelos resultados das pesquisas realizadas no <b> LabCA ⓘ - Laboratório de Análise e Criação de Ambiências </b> da Escola de Belas Artes - EBA/UFRJ - com o qual o projeto Atitudes Sustentáveis tem vínculo, dentro da interação dialógica entre ensino pesquisa e extensão, algumas dessas ações se voltam para as novas tecnologias de lâmpadas de alto rendimento luminoso, usadas no espaço interno residencial em substituição às lâmpadas incandescentes.
+                    A partir de demandas da sociedade identificadas pelos resultados das pesquisas realizadas no <span onClick={openPopupLabca}><b> LabCA ⓘ - Laboratório de Análise e Criação de Ambiências </b></span> da Escola de Belas Artes - EBA/UFRJ - com o qual o projeto Atitudes Sustentáveis tem vínculo, dentro da interação dialógica entre ensino pesquisa e extensão, algumas dessas ações se voltam para as novas tecnologias de lâmpadas de alto rendimento luminoso, usadas no espaço interno residencial em substituição às lâmpadas incandescentes.
                 </p>
 
                 <p className="atitudes-text">
-                    O aplicativo Lumos é uma iniciativa do projeto Atitudes Sustentáveis viabilizada pelo projeto de extensão <b> PROMOVE ⓘ</b> do Instituto de Computação (IC) da UFRJ. Essa parceria entre esses dois projetos de extensão da mesma Instituição Federal de Ensino Superior está alinhada com o ODS - Objetivo de Desenvolvimento Sustentável - 12 (selo ODS 12) que consiste em assegurar padrões de produção e de consumo sustentáveis, presente na Agenda 2030 da Organização das Nações Unidas, com a qual o Brasil está comprometido, e atende a política da Universidade de promover sustentabilidade através da educação.
+                    O aplicativo Lumos é uma iniciativa do projeto Atitudes Sustentáveis viabilizada pelo projeto de extensão <span onClick={openPopupDevmob}><b> PROMOVE ⓘ</b></span> do Instituto de Computação (IC) da UFRJ. Essa parceria entre esses dois projetos de extensão da mesma Instituição Federal de Ensino Superior está alinhada com o ODS - Objetivo de Desenvolvimento Sustentável - 12 (selo ODS 12) que consiste em assegurar padrões de produção e de consumo sustentáveis, presente na Agenda 2030 da Organização das Nações Unidas, com a qual o Brasil está comprometido, e atende a política da Universidade de promover sustentabilidade através da educação.
                 </p>
             </div>
+
+            <div className='popup' id='popup-labca'>
+                <InformationPopup 
+                    title={labcaTitle}
+                    text={labcaText}
+                    containerId="popup-labca"
+                    blackoutId="popup-blackout"
+                />
+            </div>
+
+            <div className='popup' id='popup-devmob'>
+                <InformationPopup 
+                    title={devmobTitle}
+                    text={devmobText}
+                    containerId="popup-devmob"
+                    blackoutId="popup-blackout"
+                />
+            </div>
+
+            <div id='popup-blackout'></div>
         </div>
     );
 };
--- src/Components/InformationPopup/InformationPopup.js
+++ src/Components/InformationPopup/InformationPopup.js
@@ -2,23 +2,32 @@ import React from 'react';
 import './InformationPopup.css';
 
 
-const InformationPopup = ({ title, text }) => {
-  return (
-    <div className='container'>
-        <div className='header'>
-            <div className='header-title-container'>
-                <p className='title'>
+const InformationPopup = ({ title, text, containerId, blackoutId }) => {  
+    const handleClick = () => {
+        document.getElementById(containerId).style.visibility = 'hidden';
+
+        if (blackoutId != null) {
+            document.getElementById(blackoutId).style.visibility = 'hidden';
+        }
+    }
+  
+    return (
+    <div className='popup-container'>
+        <div className='popup-header'>
+            <div className='popup-header-title-container'>
+                <p className='popup-title'>
                    {title}
                 </p>
             </div>
 
-            <div className='header-button-container'>
-                <p className='header-button'>✕</p>
+            <div className='popup-header-button-container'
+                 onClick={handleClick}>
+                <p className='popup-header-button'>✕</p>
             </div>
         </div>
 
-        <div className='text-container'>
-            <p className='text'>{text}</p>
+        <div className='popup-text-container'>
+            <p className='popup-text'>{text}</p>
         </div>
     </div>
   );
